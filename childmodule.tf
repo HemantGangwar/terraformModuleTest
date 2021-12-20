ce5f2f22be19a8a3197bec10019076df48158818
Hemant Gangwar <38517925+HemantGangwar@users.noreply.github.com>
2021-12-20 11:51:59 +0530

    Update childmodule.tf

--- childmodule.tf
+++ childmodule.tf
@@ -1,8 +1,9 @@
 module "ec2module" {
-        source = "/opt/terraform/modules/ec2_only/"
-       # instance_hw = "t2.nano"
+        source = "../../tmp/puppet/"
         custom_ami_id = "ami-06a0b4e3b7eb7a300"
         aws_ec2_name = var.aws_ec2_name
                 }
 
 variable "aws_ec2_name" {}
+
+output "awsip" { value = module.ec2module.awsip }
